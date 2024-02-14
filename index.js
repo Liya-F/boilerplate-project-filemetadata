929b4eb3b6ac19f4030f119155a803cb9ef73528
Liya Hbtemariam <liyahabtemariam11@gmail.com>
2024-02-14 15:16:06 -0800

    completed

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+
+// Set up the multer middleware for file upload
+const upload = multer();
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res)=>{
+  if(!req.file){
+    console.log("No file uploaded!")
+  }else{
+    console.log(req.file)
+    const { originalname, mimetype, size } = req.file;
+    res.json({ name: originalname, type: mimetype, size: size });
+  }
+});
 
-
+// Move the express.static middleware here
+app.use('/public', express.static(process.cwd() + '/public'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
